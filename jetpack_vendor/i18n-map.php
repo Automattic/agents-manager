99f4c90636e4e4a1e1a03ad77071a6a9f4acad60
anomiex <anomiex@users.noreply.github.com>
2026-06-18 18:00:02 +0000

    Update babel monorepo to v7.29.7 (#49757)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27778213438
    
    Upstream-Ref: Automattic/jetpack@154064ab4348a1546dda278c604d188669d486f1

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -10,7 +10,7 @@ return array(
     ),
     'jetpack-agents-manager' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-agents-manager',
-      'ver' => '0.5.1-alpha1781711227',
+      'ver' => '0.5.1-alpha1781804518',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.7.1-alpha1781711227',
+      'ver' => '8.7.1-alpha1781804518',
     ),
   ),
 );
