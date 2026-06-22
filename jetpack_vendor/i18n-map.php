25d9192e1570c94b9404646ded2ba2a33972dd16
anomiex <anomiex@users.noreply.github.com>
2026-06-22 18:42:38 +0000

    Backport jetpack 16.0-a.3 changes (#49821)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27974739729
    
    Upstream-Ref: Automattic/jetpack@38fa0d4b6d56ebcb5082fe1d78376645ab7981d5

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,19 +6,19 @@ return array(
   'packages' => array(
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.9.4-alpha1781711227',
+      'ver' => '0.9.4',
     ),
     'jetpack-agents-manager' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-agents-manager',
-      'ver' => '0.5.1-alpha1781860410',
+      'ver' => '0.5.1',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.4.0-alpha1781793944',
+      'ver' => '4.4.0',
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.7.1-alpha1782139228',
+      'ver' => '8.7.2-alpha1782152687',
     ),
   ),
 );
