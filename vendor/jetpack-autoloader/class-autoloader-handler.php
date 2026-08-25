0f6b0e736edf2d73b8ffb887fc831b95309add2e
anomiex <anomiex@users.noreply.github.com>
2026-08-25 17:34:02 +0000

    Update minimum PHP version to 7.4 (#51515)
    * Bump minimum version in `.github/versions.sh`.
    * Drop PHP Lint 7.2 job.
    * Update packages' `.require.php` where applicable.
    * Update plugin "Requires PHP" headers.
    * Remove PHPUnit 8 stuff. PHP 7.4 uses PHPUnit 9.
      * Also, fix some PHPUnit 12 configs that should be symlinks.
    * Update some deps, doc refs, and constants.
    
    Updating PHPCS, Phan, and cleaning up now-obsolete code will be left for followup PRs.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/32876309789
    
    Upstream-Ref: Automattic/jetpack@3ac78c4bac66d6513d02e8ff692f9cca5e79d9eb

--- vendor/jetpack-autoloader/class-autoloader-handler.php
+++ vendor/jetpack-autoloader/class-autoloader-handler.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpa0c8b4b8a21285954dcfaae4c85e8ab2_agents_managerⓥ0_1_3_alpha\al5_0_23;
+namespace Automattic\Jetpack\Autoloader\jpa0c8b4b8a21285954dcfaae4c85e8ab2_agents_managerⓥ0_1_3_alpha\al6_0_0_alpha;
 
  // phpcs:ignore
 
